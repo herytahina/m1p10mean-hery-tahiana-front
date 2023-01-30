7674abcc1d2081c6fca62b42fede12e5bfa36335
Hery Tahina Rakotonaivo <heryrak.hr@gmail.com>
2023-01-30 11:35:13 +0300

    fix modal integration

--- src/app/shared/modal-basic/modal-basic.component.ts
+++ src/app/shared/modal-basic/modal-basic.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, Input, Output, ViewEncapsulation, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-modal-basic',
@@ -10,8 +10,9 @@ export class ModalBasicComponent implements OnInit {
   @Input() dialogClass: string;
   @Input() hideHeader = false;
   @Input() hideFooter = false;
-  public visible = false;
-  public visibleAnimate = false;
+  @Input() visible = false;
+  @Input() visibleAnimate = false;
+  @Output() close = new EventEmitter();
 
   constructor() {}
 
@@ -19,19 +20,9 @@ export class ModalBasicComponent implements OnInit {
 
   }
 
-  public show(): void {
-    this.visible = true;
-    setTimeout(() => this.visibleAnimate = true, 100);
-  }
-
-  public hide(): void {
-    this.visibleAnimate = false;
-    setTimeout(() => this.visible = false, 300);
-  }
-
   public onContainerClicked(event: MouseEvent): void {
     if ((<HTMLElement>event.target).classList.contains('modal')) {
-      this.hide();
+      this.close.emit('close');
     }
   }
 
